80c19f3366c8cfe8b599c18830a0b9a2258a4d42
Unknown <leonis13579@gmail.com>
2019-05-04 21:08:35 +0300

    Client App added
    Была добавлена последняя версия клиентского приложения на WPF. Проект не трогался как пол года

---
+++ ClientApp/ExpHub_3.0/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExperenceHubApp
+{
+    public class Person
+    {
+        public string firstname;
+        public string lastname;
+        public float wallet;
+        public List<Lesson> Lessons;
+        public string login;
+        public string email;
+        public string password;
+        public string token;
+        public Guid userid;
+        public string NewPassword;
+        public string role;
+
+        [OnDeserialized]
+        internal void OnDeserializedMethod(StreamingContext context)
+        {
+            if (Lessons == null)
+            {
+                Lessons = new List<Lesson>();
+            }
+        }
+    }
+}
